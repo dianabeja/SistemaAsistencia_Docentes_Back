19d34ffa0d5d55e8b15bba063663fd7b675b493d
Diana Bautista <bagdiana03@gmail.com>
2023-11-07 13:30:59 -0600

    cambios

--- src/Connection/Database/connection.js
+++ src/Connection/Database/connection.js
@@ -3,17 +3,17 @@ const { Pool } = pkg;
 
 // Configuración de la base de datos
 const pool = new Pool({
-  //user: 'irving.condem',
-  //password: 'ROLfb7mx3gQl',
-  //host: 'ep-plain-bar-23424588.us-east-2.aws.neon.tech',
-  //port: 5432,
-  //database: 'student',
-  //ssl: {
-  //  rejectUnauthorized: false,
-  //},
-  //connectionConfig: {
-  //  charset: 'UTF8'
-  //},
+  user: 'irving.condem',
+  password: 'ROLfb7mx3gQl',
+  host: 'ep-plain-bar-23424588.us-east-2.aws.neon.tech',
+  port: 5432,
+  database: 'student',
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  connectionConfig: {
+    charset: 'UTF8'
+  },
   //user: 'postgres',
   //host: 'localhost',
   //database: 'SIAEFINAL',
